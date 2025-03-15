4b6e6303b71acb87407aa20f00dd031ebe126674
dhew0016 <dhew0016@student.monash.edu>
2025-03-15 15:51:58 +1100

    Created links to each web page on the index.html file.

--- main.py
+++ main.py
@@ -6,6 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/jobTracker")
+def jobTracker():
+    return render_template("job-tracker.html")
+
+@app.route("/eventFinder")
+def eventFinder():
+    return render_template("event-finder.html")
+
+@app.route("/careerRecommender")
+def careerRecommender():
+    return render_template("career-recommender.html")
+
+@app.route("/resumeBuilder")
+def resumeBuilder():
+    return render_template("resume-builder.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
